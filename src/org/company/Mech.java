3815d6c6aca347634fc5860214539d241811febe
balajig1606 <gbalaji1606@gmail.com>
2019-08-01 09:26:09 +0530

    Arul code added

---
+++ src/org/company/Mech.java
@@ -0,0 +1,13 @@
+package org.company;
+
+public class Mech {
+		public void companyName() {
+			System.out.println("My Company name is CTS");
+		}
+		public static void main(String[] args) {
+			Company c=new Company();
+			c.companyName();
+			c.clientName();
+			
+		}
+}
